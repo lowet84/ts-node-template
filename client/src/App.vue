92020162b7a11c5bd0b2939ce58926400bd0237b
Fredrik Löwenhamn <lowet84@gmail.com>
2018-06-08 10:48:56 +0200

    Minor settings changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,16 +6,16 @@
 </template>
 
 <script>
-import Vue from "vue";
+import Vue from 'vue'
 
 export default Vue.extend({
-  name: "app"
-});
+  name: 'app'
+})
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
